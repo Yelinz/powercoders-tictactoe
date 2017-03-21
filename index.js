b9b30cb791fac4fdf27aa2112cd013bdac550ec0
Christian Zosel <christian@zosel.ch>
2017-03-21 22:53:59 +0100

    Style fixes in index.js

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var $ = require('jquery');
-var tictactoe = require('./tictactoe');
+var $ = require("jquery");
+var tictactoe = require("./tictactoe");
 
 var last;
 var state;
 var winner;
 
+$(function() {
+  initializeGame();
+
+  $("td").click(cellClicked);
+});
+
 function initializeGame() {
   last = "X";
   state = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
   ];
-  $('td').html('');
-}
-
-function isDraw() {
-  // if the game board is full, but there is no winner, it is a draw!
-  return $("td:empty").length == 0;
+  $("td").html("");
 }
 
 function cellClicked() {
-  var row = $(this).parent().attr('data-row');
-  var col = $(this).attr('data-col');
+  var row = $(this).parent().attr("data-row");
+  var col = $(this).attr("data-col");
 
   if (!$(this).text()) {
-    if (last === "X") {
-      $(this).text('O');
-      last = "O";
-    } else {
-      $(this).text('X');
-      last = "X";
-    }
+    last = (last === "X") ? "O" : "X";
 
+    // update UI
+    $(this).text(last);
+
+    // update state
     state[row][col] = last;
 
+    // check if game is over
     winner = tictactoe.check(state);
     if (winner || isDraw()) {
       alert(tictactoe.format(winner));
@@ -43,9 +43,10 @@ function cellClicked() {
   }
 }
 
-$(function() {
-
-  initializeGame();
-
-  $('td').click(cellClicked);
-});
+/**
+ * If the game board is full, but there is no winner,
+ * it is a draw!
+ */
+function isDraw() {
+  return $("td:empty").length == 0;
+}
